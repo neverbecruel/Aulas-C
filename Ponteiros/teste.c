00e6d3a00e98c3db256cfd3d8e4a7a7b79b779bd
Victor <pipoca.play19@gmail.com>
2024-06-04 22:03:22 -0300

    aula Ponteiros

--- Ponteiros/teste.c
+++ Ponteiros/teste.c
@@ -1,9 +1,19 @@
 #include <stdio.h>
 
+int print(int num, int somador) {
+    int total = num + somador;
+    return total;
+
+}
+
 int main(){
-    int vetor[10] = {1,2,3,4,5,6,7,8,9,10};
+    /*int vetor[10] = {1,2,3,4,5,6,7,8,9,10};
     printf("1 ->%x\n", &vetor[0]); // 15e7ff6f0
     printf("2 ->%x\n", &vetor);// 25e7ff6f0
-    printf("3 ->%x", vetor);// 35e7ff6f0
+    printf("3 ->%x", vetor);// 35e7ff6f0*/
+    int d = 0;
+    print(d);
+    printf("Fora da funcao ->%x\n", d);
+
 
 }
--- Stacks/Pilhas1.0.c
+++ Stacks/Pilhas1.0.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include <stdlib.h>
+
 
 //---------------------------------------------------------------- %d/%i => inteiros
 //---------------------------------------------------------------- %p => endereço de memória
@@ -9,64 +9,76 @@
 
 #define STACKSIZE 100
 
-struct stack{
-    int top;
-    int itens[STACKSIZE];
+
+//PILHAS LIFO
+
+/*
+    Pilhas LIFO são uma estrutura de dados na qual novos itens podem ser colocado ou retirados de uma --apenas uma--
+ das suas extremidades. Essa extremidade chamamos de TOPO.
+  */
+
+struct pilha {
+    int itens[STACKSIZE]; // Para criar a pilha LIFO, iniciamos um vetor que servirá de casa para a pilha.
+    int top; // E aqui definimos seu topo.
 };
 
-int empty(struct stack *ps){
-    if (ps->top == -1){
-      return 1;
-    }else{
+// Para manipularmos a nossa pilha LIFO, precisaremos de algumas funções.
+
+int isEmpty(struct pilha *ps) { // Com essa função, conferimos se a pilha LIFO está vazia.
+    if (ps->top == -1) {
+        return 1;
+    } else {
         return 0;
     }
 }
 
-int pop(struct stack *ps) {
-    if (!empty(ps)) {
-        int valor = ps->itens[ps->top];
-        ps->top--;
-        return valor;
+int push(int x, struct pilha *ps) { // Com essa função, inserimos um item na pilha LIFO.
+    if (ps->top == STACKSIZE - 1) { // Antes de inserir um novo item, precisamos nos certificar de que há espaço.
+        printf("A pilha está cheia.\n");
+        return -1;
     } else {
-        printf("A pilha está vazia...");
+        ps->itens[++ps->top] = x; // Caso haja espaço, inserimos o item na pilha LIFO.
+        printf("O número %d foi adicionado com sucesso.\n", ps->itens[ps->top]);
+        return 0;
+    }
+}
+
+int pop(struct pilha *ps) { // Com essa função, excluimos o ULTIMO elemento da pilha LIFO
+    if (!isEmpty(ps)) { // Checamos se a pilha está vazia.
+        return ps->itens[ps->top--]; // Se não retornamos o valor do ultimo elemento e decrementamos o valor do topo.
+    } else {
+        printf("A pilha está vazia.\n");
         return -1;
     }
 }
-int push(struct stack *ps, int num){
-    if (ps->top < STACKSIZE - 1) {
-        ps->top++;
-        ps->itens[ps->top] = num;
-        //printf("Item %d adicionado à pilha", num);
-    }else{
-        printf("A pilha está cheia...");}
+
+int showLast(struct pilha *ps) { // Com essa função, retornamos o ultimo elemento da pilha.
+    if (!isEmpty(ps)) {
+        return ps->itens[ps->top]; // Retornamos o valor no vetor cujo índice corresponde a ps->top
+    } else {
+        printf("A pilha está vazia.\n");
+        return -1;
     }
-
-
-
-
-
-
-
-
-int main(){
-    struct stack s;
-    struct stack *ptrstack;
-    ptrstack = &s;
-    ptrstack->top = -1;
-
-    printf("A pilha está vazia? %s\n", empty(ptrstack)?"Sim" : "Não");
-
-    push(ptrstack, 10);
-    push(ptrstack, 3);
-    push (ptrstack,22);
-
-    printf("A pilha está vazia? %s\n", empty(ptrstack)?"Sim" : "Não");
-
-    for (int item = 0 ; item < (ptrstack->top); item++){
-        printf("%d \n", s.itens[item]);
-    }
-    printf("\n");
-    printf("O número %d foi retirado.", pop(ptrstack));
-
-
 }
+
+
+int main() {
+    struct pilha ps; // Declaramos uma pilha LIFO
+    ps.top = -1; // Definimos -1 como valor inicial para ps.top
+
+    pop(&ps); // RETORNO: A pilha está vazia.
+    push(22, &ps); // RETORNO: O número 22 foi adicionado com sucesso.
+    int last = showLast(&ps);
+    printf("O último número da pilha é:%d\n", last); // RETORNO: O último elemento da pilha é: 22
+    int num = pop(&ps);
+    printf("O número %d foi excluido.\n", num); // RETORNO: O número 22 foi excluido.
+    last = showLast(&ps);
+    printf("O último número da pilha é:%d\n", last); // RETORNO: O último elemento da pilha é: -1
+    printf("%d\n", ps.top); // RETORNO: O-1
+}
+/* OBSERVAÇÕES
+
+    Por padrão os parametros de função em C são passados por valor, isto é, a função cria uma cópia desse valor que só
+ existe dentro da própria função.
+    Quando queremos passar uma estrutura como parametro, passamos seu endereço.
+*/
